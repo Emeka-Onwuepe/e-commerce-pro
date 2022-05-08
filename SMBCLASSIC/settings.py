9df39dd570dac37c1606ebc1c26d71471e426fc5
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2022-05-08 17:37:55 +0100

    added allowed host

--- SMBCLASSIC/settings.py
+++ SMBCLASSIC/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY_8")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','http://127.0.0.1:8000','*']
+ALLOWED_HOSTS = ['127.0.0.1','http://127.0.0.1:8000','https://stocksaleapp.herokuapp.com']
 
 
 # Application definition
